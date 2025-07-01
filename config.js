ea10f8cd980742a852ccc5c0dc108faad2e45de2
pixumi <dharmasugiyanto224@gmail.com>
2025-07-01 15:22:24 +0800

    update keyword

--- config.js
+++ config.js
@@ -114,7 +114,7 @@ const allowedDoneSenders = new Set([
 const activityKeywords = [
   { 
     category: 'MAINTAIN', 
-    keywords: ['pl04','found','sloc','maintananc3','maintenace','storage location','mainte','maintaince','maibnten','mantain','mentenance','maintenenkn','mentenace','maintennace','maintained','maintece','mainten','menten','maintian','maintain','maintanance','maintannace','maintenance','maiantan','maintan','maintence','maintance','maintened','maintanace','maitnenacne','maintenacne','bin','update'] 
+    keywords: ['pl04','found','sloc','maintananc3','maintenace','storage location','mainte','maintaince','maibnten','mantain','mentenance','maintenenkn','mentenace','maintennace','maintaine','maintained','maintece','mainten','menten','maintian','maintain','maintanance','maintannace','maintenance','maiantan','maintan','maintence','maintance','maintened','maintanace','maitnenacne','maintenacne','bin','update'] 
   },
   { 
     category: 'BLOK/OPEN BLOCK', 
